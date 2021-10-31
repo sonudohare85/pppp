26f30b4cb2f1499400b9e7a0a871313603c9e38f
ACE <91398173+imacekun@users.noreply.github.com>
2021-11-01 02:22:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import requests
 from flask import Flask
 from flask import request
 from flask import render_template
-from werkzeug.utils import redirect
-from youtube_dl import YoutubeDL
+
 
 
 ACCOUNT_ID = os.environ.get("ACCOUNT_ID", "6206459123001")
